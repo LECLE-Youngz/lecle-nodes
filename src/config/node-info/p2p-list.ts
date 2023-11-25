a352838389e38bf1a1f6690765272526ce2afd4f
Kiet1618 <ngokiet3207@gmail.com>
2023-11-25 19:26:33 +0700

    fix grpc

--- src/config/node-info/p2p-list.ts
+++ src/config/node-info/p2p-list.ts
@@ -3,12 +3,12 @@ dotenv.config()
 
 export default {
   "node1": {
-    "url": process.env.NODE1_URL
+    "url": process.env.NODE1_URL || "127.0.0.1:4001"
   },
   "node2": {
-    "url": process.env.NODE2_URL
+    "url": process.env.NODE2_URL || "127.0.0.1:4002"
   },
   "node3": {
-    "url": process.env.NODE3_URL
+    "url": process.env.NODE3_URL || "127.0.0.1:4003"
   }
 }
