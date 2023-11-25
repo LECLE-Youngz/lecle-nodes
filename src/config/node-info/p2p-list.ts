334e81cadd78a82e4fd4e15b8ec1d331dd14a856
Kiet1618 <ngokiet3207@gmail.com>
2023-11-25 20:34:39 +0700

    remove env

--- src/config/node-info/p2p-list.ts
+++ src/config/node-info/p2p-list.ts
@@ -1,14 +1,11 @@
-import * as dotenv from "dotenv"
-dotenv.config()
-
 export default {
   "node1": {
-    "url": process.env.NODE1_URL
+    "url": "127.0.0.1:4001"
   },
   "node2": {
-    "url": process.env.NODE2_URL
+    "url": "127.0.0.1:4002"
   },
   "node3": {
-    "url": process.env.NODE3_URL
+    "url": "127.0.0.1:4003"
   }
 }
